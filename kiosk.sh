753bcb3e81c9bb471e17109cfdecf906bcfb9987
Ricardo Fernandes <37550826+ricardofrfernandes@users.noreply.github.com>
2024-09-04 17:30:56 +0100

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sleep 10 # Wait for 10 seconds to ensure network is up
+sleep 30 # Wait for 10 seconds to ensure network is up
 chromium-browser --start-fullscreen --noerrdialogs --disable-infobars --kiosk https://www.turno24.com
